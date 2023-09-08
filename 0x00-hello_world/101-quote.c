037d1e8926366e2dc0829ce84da6b0414faded3c
Ahmed <sherwedahmed1@gmail.com>
2023-09-08 03:30:57 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: this program will print string with printf fun. and return 1
+ *
+ * Return: Always 1 (Erorr)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+		return (1);
+}
