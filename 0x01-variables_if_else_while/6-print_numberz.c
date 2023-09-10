b669bd9eab5fcd6b4fa3542c967b067b06a9e20a
Ahmed <sherwedahmed1@gmail.com>
2023-09-10 05:07:29 -0700

    numz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: True or False validation
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 57)
+	{
+		putchar(digit);
+		digit++;
+	}
+	printf("\n");
+	return (0);
+}
