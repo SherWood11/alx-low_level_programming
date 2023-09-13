eaba2fa30bb2cc0791e8fdc0faf8c25696c83164
Ahmed <sherwedahmed1@gmail.com>
2023-09-13 15:38:01 -0700

    Fitst stemp

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: print _putchar
+ *
+ * Return: Always 0 (success)
+ * 
+ */
+int main(void)
+{
+	char str[] = "putchar";
+	int ch;
+	
+	for (ch = 0; ch < 8; ch++)
+	{
+		_putchar(str[c]);
+		_putchar('\n');
+		return (0);
+	}
+}
