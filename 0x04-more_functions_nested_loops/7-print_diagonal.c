ffc1ae38fa5cce845c7f2ab3cff3fd9dc1e40313
Ahmed <sherwedahmed1@gmail.com>
2023-09-15 18:13:23 -0700

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_digonal - print a digonal line
+ *
+ * @n: it is number of times should print
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int postn, space;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; postn++)
+			_putchar(' ');
+		_putchar(92); /*char '/'*/
+		_putchar('\n');
+	}
+}
