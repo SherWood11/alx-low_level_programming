675bb549a3d9c466bc72b15a9361f83262a9b861
Ahmed <sherwedahmed1@gmail.com>
2023-10-25 12:46:45 -0700

    ada

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -19,10 +19,8 @@ size_t print_listint_safe(const listint_t *head)
 		if (diff > 0)
 			head = head->next;
 		else
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			return (98);
-		}
+
 	}
 
 	return (num);
