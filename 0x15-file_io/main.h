2dd93b0001ccd6806635114e5c8b1c915415308c
Ahmed <sherwedahmed1@gmail.com>
2023-10-31 14:19:08 -0700

    as

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,20 @@
 #ifndef MAIN_H
-#define MAIN_N
+#define MAIN_H
 
-#include <elf.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
+/*
+ * File: holberton.h
+ * Auth: Godswill Kalu
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-#endif
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif /* MAIN_H */
