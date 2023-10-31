78f2e846e624aa42a4fad4d5869cebdb3e7b21f3
Ahmed <sherwedahmed1@gmail.com>
2023-10-31 14:23:01 -0700

    ed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,24 +10,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int of, wf, lenf = 0;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (lenf = 0; text_content[len];)
-			lenf++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	of = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wf = write(of, text_content, lenf);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (of == -1 || wf == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(of);
+	close(o);
 
 	return (1);
 }
